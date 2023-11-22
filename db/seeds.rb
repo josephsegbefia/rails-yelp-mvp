f5c20dd01f21e9e7ff9fbb3e1186d3d0149a6ba2
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-11-22 15:22:28 +0000

    seed the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,23 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Flushing the database.."
+Restaurant.destroy_all
+puts "Flushed"
+
+puts "Building the restaurants one brick at a time"
+restaurants = [
+  {name: "Pizza Inn", address: "84 New Road Accra", phone_number: "+233209990099", category: "belgian"},
+  {name: "Star Bites", address: "Dansoman, Sahara", phone_number: "+2332093099", category: "french"},
+  {name: "Lord of the Wings", address: "Cantoments", phone_number: "+233249580093", category: "japanese"},
+  {name: "Breakfast2Breakfast", address: "Osu, Accra", phone_number: "+233209340099", category: "italian"},
+  {name: "Chicken Man", address: "Dansoman Exhibiton", phone_number: "+233204590595", category: "chinese"},
+]
+
+restaurants.each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Finished building #{restaurant.name} ready to take orders"
+end
+
+puts "All restaurants ready!!"
