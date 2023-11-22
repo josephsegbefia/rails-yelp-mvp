78b887e5123138053bf7516b7a8b03c04fb1012d
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-11-22 23:56:59 +0000

    Done, but styles  could be better

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_22_145149) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_22_214038) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_145149) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 
   create_table "reviews", force: :cascade do |t|
